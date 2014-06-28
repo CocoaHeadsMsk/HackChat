cbb8048ce8b7af00f9351fce293c90e23c0ee145
DanSkeel <danskeel@gmail.com>
2014-06-28 19:22:52 +0400

    initial

---
+++ HackChat/HackChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackChat
+//
+//  Created by Danila Shikulin on 28/6/14.
+//  Copyright (c) 2014 CHM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
